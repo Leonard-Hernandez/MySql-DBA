1317b0210424e2f8d33044aa89919c5c835158d1
leonard <leonardhernandez02@gmail.com>
2023-11-23 15:47:32 -0500

    firts commit

--- Administracion de bases de datos.sql
+++ Administracion de bases de datos.sql
@@ -1,2 +1,11 @@
 /*Muestra las tablas que estan creadas en el disco*/
 show global status like 'Created_tmp_disk_tables';
+
+/*Muestra las tablas creadas en ram*/
+show global status like 'Created_tmp_tables';
+
+/*Ver la cantidad de memoria asignada a las tablas temporales*/
+show global variables like 'tmp_table_size';
+
+/*Cambiando el valor de la variable*/
+set global tmp_table_size = 136700160;
